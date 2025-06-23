20925573ffd35cf1a32477b489efdb9a3e8453cc
Adimula Ravi Shankar <ravishankaradimula2004@gmail.com>
2025-06-23 17:39:33 +0530

    users page for admin is compeleted

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,15 +14,21 @@ import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
 
 import Profile from "./pages/User/Profile.jsx";
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />}></Route>
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile></Profile>}></Route>
       </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />}></Route>
+      {/* //Admin router */}
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />}></Route>
+      </Route>
     </Route>
   )
 );
