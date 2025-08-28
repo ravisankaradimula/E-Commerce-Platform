23cdf59c707462d00ddaa8771a71b571040d21c9
Adimula Ravi Shankar <ravishankaradimula2004@gmail.com>
2025-08-29 00:42:24 +0530

    filter by categories is added

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -16,7 +16,7 @@ const Shop = () => {
   );
 
   const categoriesQuery = useFetchCategoriesQuery();
-  const [priceFilter, setPriceFilter] = useSelector("");
+  const [priceFilter, setPriceFilter] = useState("");
 
   const filteredProductsQuery = useGetFilteredProductsQuery({ checked, radio });
 
@@ -40,7 +40,7 @@ const Shop = () => {
         dispatch(setProducts(filteredProducts));
       }
     }
-  });
+  }, [checked, radio, filteredProductsQuery.data, dispatch, priceFilter]);
 
   const handleBrandClick = (brand) => {
     const productByBrand = filteredProductsQuery.data?.filter(
@@ -66,6 +66,48 @@ const Shop = () => {
     ),
   ];
 
+  const handlePriceChange = (e) => {
+    setPriceFilter(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="flex md:flex-row">
+          <div className="bg-[#151515] p-3 mt-2 mb-2">
+            <h2 className="h4 text-center py-2 bg-black rounded-full mb-2">
+              Filter by Categories
+            </h2>
+
+            <div className="p-5 w-[15rem]">
+              {categories?.map((c) => (
+                <div key={c._id} className="mb-2">
+                  <div className="flex items-center mr-4">
+                    <input
+                      type="checkbox"
+                      id="red-checkbox"
+                      onChange={(e) => handleCheck(e.target.value, c._id)}
+                      className="w-4 h-4 text-pink-600 
+                      bg-gray-100 border-gray-300 rounded focus:ring-pink-500 
+                      dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600
+                      checked:bg-pink-600 checked:border-transparent"
+                    />
+                    <label
+                      htmlFor="pink-checkbox"
+                      className="ml-2 text-sm font-medium text-white dark:text-gray-300"
+                    >
+                      {c.name}
+                    </label>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
   return <div>Shop</div>;
 };
+
 export default Shop;
